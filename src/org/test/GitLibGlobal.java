dc264cabd107e3cef016dc4a285e50125ec1b605
Ajithkumar1712 <ajithsundaramoorthy1712@gmail.com>
2021-08-24 12:00:18 +0530

    LoadUrl Done

--- src/org/test/GitLibGlobal.java
+++ src/org/test/GitLibGlobal.java
@@ -1,5 +1,14 @@
 package org.test;
 
 public class GitLibGlobal {
+	public void LoadUrl() {
+		System.out.println("load url");
+
+	}
+	
+	public static void main(String[] args) {
+		GitLibGlobal b = new GitLibGlobal();
+		b.LoadUrl();
+	}
 
 }
